661cf2e3a27b017e442ffdf1c6e99b95b16f991f
IlyaPodkolzin <pdk.04@mail.ru>
2021-02-27 20:48:43 +0300

    /promotion_image

--- main.py
+++ main.py
@@ -34,5 +34,40 @@ def image_mars():
                 </html>"""
 
 
+@app.route("/promotion_image")
+def promotion_image():
+    return """<!doctype html>
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                         crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
+                        <title>Колонизация</title>
+                    </head>
+                    <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="/static/img/mars.jpg">
+                        <div class="alert alert-dark" role="alert">
+                         <h4>Человечество вырастает из детства.</h4>
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                         <h4>Человечеству мала одна планета.</h4>
+                        </div>
+                        <div class="alert alert-dark" role="alert">
+                         <h4>Мы сделаем обитаемыми безжизненные пока планеты.</h4>
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                         <h4>И начнём с Марса!</h4>
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                         <h4>Присоединяйся!</h4>
+                        </div>
+                    </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
