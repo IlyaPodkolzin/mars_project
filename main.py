b9bb83e870eedf8881890eb6056d31639e3f8ae1
IlyaPodkolzin <pdk.04@mail.ru>
2021-03-06 00:15:31 +0300

    /choice

--- main.py
+++ main.py
@@ -181,5 +181,39 @@ def astronaut_selection():
                     </html>"""
 
 
+@app.route("/choice/<planet_name>")
+def choice(planet_name):
+    return f'''<!doctype html>
+                   <html lang="en">
+                       <head>
+                          <meta charset="utf-8">
+                          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet"
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                       crossorigin="anonymous">
+                           <title>Варианты выбора</title>
+                       </head>
+                       <body>
+                           <h1>Моё предложение: {planet_name}</h1>
+                           <div class="alert alert-light" role="alert">
+                               <h4>Эта планета близка к Земле;</h4>
+                           </div>
+                           <div class="alert alert-success" role="alert">
+                               <h4>На ней много необходимых ресурсов;</h4>
+                           </div>
+                           <div class="alert alert-dark" role="alert">
+                               <h4>На ней есть вода и атмосфера;</h4>
+                           </div>
+                               <div class="alert alert-warning" role="alert">
+                           <h4>На ней есть небольшое магнитное поле</h4>
+                           </div>
+                           <div class="alert alert-danger" role="alert">
+                               <h4>Наконец, она просто красива!</h4>
+                           </div>
+                       </body>
+                   </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
