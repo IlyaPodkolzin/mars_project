377925849d0011e3fbe21f9957c985d325e736dc
IlyaPodkolzin <pdk.04@mail.ru>
2021-03-12 22:56:26 +0300

    /carousel

--- main.py
+++ main.py
@@ -244,5 +244,49 @@ def results(nickname, level, rating):
                    </html>'''
 
 
+@app.route("/carousel")
+def carousel():
+    return '''<!doctype html>
+                  <html lang="en">
+                      <head>
+                          <meta charset="utf-8">
+                          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                      <link rel="stylesheet"
+                      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                      integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                      crossorigin="anonymous">
+                          <title>Пейзажи Марса</title>
+                      </head>
+                      <body>
+                          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+                          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+                          <center><h1>Пейзажи Марса</h1></center>
+                          <div class="container">
+                              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                  <div class="carousel-inner">
+                                      <div class="carousel-item active">
+                                          <img src="static/img/mars1.jpg" class="d-block w-100">
+                                      </div>
+                                      <div class="carousel-item">
+                                          <img src="static/img/mars2.jpg" class="d-block w-100">
+                                      </div>
+                                      <div class="carousel-item">
+                                          <img src="static/img/mars3.jpg" class="d-block w-100">
+                                      </div>
+                                  </div>
+                                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+                                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                      <span class="visually-hidden">Previous</span>
+                                  </button>
+                                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+                                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                      <span class="visually-hidden">Next</span>
+                                  </button>
+                              </div>
+                          </div>
+                      </body>
+                  </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
